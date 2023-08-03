6c5c6f74d6b53f2638d476de58b6fd9d72a787ef
Flavio Gonzalo Villalba Gargantini <gonzagargantini20@gmail.com>
2023-08-03 07:36:05 -0300

    inicio Test

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,9 +1,14 @@
-
+import PropTypes from 'prop-types';
 export const GifItem = ({url, title}) => {
   return (
     <div className="card">
-        <img src={url} alt={title} />
-        <h4>{title}</h4>
+        <img src={ url } alt={ title } />
+        <p>{ title }</p>
     </div>
   )
+}
+
+GifItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 }
---
+++ tests/components/GifItem.test.jsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import { GifItem } from "../../src/components";
+
+describe('Prueba en <GifItem/>', () => {
+    const title = 'Saitama';
+    const url = 'https://one-punch.com/saitama.jpg'
+    test('debe hacer march contra el snapshot', () => {
+        
+        const { container } = render( <GifItem title={ title } url={ url }/> );
+        expect ( container ).toMatchSnapshot();
+    });
+    test('debe de mostrar la imagen con el URL y el ALT indicado', () => {
+        render(<GifItem title={ title } url={ url }/>);
+        const{ src, alt } = screen.getByRole('img');
+        expect( src ).toBe( url );
+        expect( alt ).toBe( alt );
+    });
+});
